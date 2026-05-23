0b65be77d490b61c66c36be55dd43d6ff921a0b4
kevmongare <kevin.n.mongare@gmail.com>
2026-05-23 23:37:32 +0300

    chore: add official vite gitignore layout

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/techyworldai/",
-  server: {
-    port: 3000,
-    open: true,
-  },
+  // server: {
+  //   port: 3000,
+  //   open: true,
+  // },
 });
